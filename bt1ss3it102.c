f87f1d63c22a23c37f2dbdee85836944a6463544
langhongphuc <langphuc27@gmail.com>
2025-10-26 18:34:24 +0700

    LangHongPhuc_B25DTCN199_IT102_SS3_BTVN1

---
+++ bt1ss3it102.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <string.h>
+
+int main () {
+	char name [10];
+	
+	printf("Vui long nhap ten cua ban: ");
+	scanf("%s",name);
+	
+	printf("Xin chao %s\n",name);
+	
+	return 0; 
+}
